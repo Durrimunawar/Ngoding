572de3fd4f02560c6891ce72a2cdb3bdeef9ee87
Durrimunawar <durrimunawar@gmail.com>
2023-07-13 09:51:14 +0700

    latihan kendali

--- latihan.java
+++ latihan.java
@@ -1,20 +1,33 @@
 public class latihan {
     public static void main(String[] args) {
-        int nialaiakhir = 80;
+        int nialaiakhir = 60;
         char grade ; 
 
 
         // kondisi
        if (nialaiakhir >= 80) {
         grade = 'A';
-        System.out.println("niai anda adlah : " + grade);
+        
+       }else if (nialaiakhir < 80 && nialaiakhir >= 70){
+        grade ='B';
 
+       }else if(nialaiakhir < 70 && nialaiakhir >= 60){
+        grade = 'C';
 
        }else{
-        System.out.println("anda bukan mahasiswa berrestasi ");
-
+        grade = 'F';
 
        }
+// ini kondisi untuk mengeluarkan output
+
+    if(grade == 'F'){
+        System.out.println("anda gagal");
+
+       }else{
+        System.out.println("nilai anada adalah");
+
+       }
+
 
        
 
