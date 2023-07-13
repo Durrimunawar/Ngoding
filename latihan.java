c6741691a83868eb38ca2aff3e0d608d8c6076df
Durrimunawar <durrimunawar@gmail.com>
2023-07-13 09:34:02 +0700

    latihan

--- latihan.java
+++ latihan.java
@@ -1,5 +1,21 @@
 public class latihan {
     public static void main(String[] args) {
+        int nialaiakhir = 80;
+        char grade ; 
+
+
+        // kondisi
+       if (nialaiakhir >= 80) {
+        grade = 'A';
+        System.out.println("niai anda adlah : " + grade);
+
+
+       }else{
+        System.out.println("anda bukan mahasiswa berrestasi ");
+
+
+       }
+
        
 
 } 
